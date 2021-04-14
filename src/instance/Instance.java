5c53c674e59afb3a9193070864221b147957616a
APreclin <amaurypreclin@gmail.com>
2021-04-14 14:19:29 +0200

    class instance

--- src/instance/Instance.java
+++ src/instance/Instance.java
@@ -1,13 +1,25 @@
 package instance;
 
+import java.util.LinkedList;
+
+import instance.reseau.Location;
+import instance.reseau.Machine;
+import instance.reseau.Technician;
+import instance.reseau.Truck;
+import solution.Request;
+
 public class Instance {
-    // Attributs
     private String dataset;
     private String name;
     private int days;
     private int technicianDistanceCost;
     private int technicianDayCost;
     private int technicianCost;
+    private LinkedList<Technician> technicians;
+    private LinkedList<Location> locations;
+    private LinkedList<Machine> machines;
+    private LinkedList<Request> requests;
+    private Truck truck;
 
     public Instance() {
         this.dataset = "";
@@ -16,6 +28,11 @@ public class Instance {
         this.technicianDistanceCost = 0;
         this.technicianDayCost = 0;
         this.technicianCost = 0;
+        this.technicians = new LinkedList<Technician>();
+        this.locations = new LinkedList<Location>();
+        this.machines = new LinkedList<Machine>();
+        this.requests = new LinkedList<Request>();
+        this.truck = new Truck();
     }
 
     public String getDataset() {
@@ -40,5 +57,33 @@ public class Instance {
 
     public int getTechnicianCost() {
         return technicianCost;
+    }
+
+    public LinkedList<Technician> getTechnicians() {
+        return new LinkedList<Technician>(technicians);
+    }
+
+    public LinkedList<Location> getLocations() {
+        return new LinkedList<Location>(locations);
+    }
+
+    public LinkedList<Machine> getMachines() {
+        return new LinkedList<Machine>(machines);
+    }
+
+    public LinkedList<Request> getRequests() {
+        return new LinkedList<Request>(requests);
+    }
+
+    public Truck getTruck() {
+        return truck;
+    }
+
+    @Override
+    public String toString() {
+        return "Instance [dataset=" + dataset + ", days=" + days + ", locations=" + locations + ", machines=" + machines
+                + ", name=" + name + ", requests=" + requests + ", technicianCost=" + technicianCost
+                + ", technicianDayCost=" + technicianDayCost + ", technicianDistanceCost=" + technicianDistanceCost
+                + ", technicians=" + technicians + ", truck=" + truck + "]";
     }
 }
