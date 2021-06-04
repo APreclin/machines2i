989ccf39de00ca280c77500218aafb1db44650a6
HugoPolard <hugo.polard@ig2i.centralelille.fr>
2021-06-04 11:25:31 +0200

    empty

--- src/io/SolutionWriter.java
+++ src/io/SolutionWriter.java
@@ -49,7 +49,7 @@ public class SolutionWriter {
             bw.write("TECHNICIAN_DISTANCE = " + solution.getTechnicianDistance() + "\n");
             bw.write("NUMBER_OF_TECHNICIAN_DAYS = " + solution.getNbTechniciansDays() + "\n");
             //TODO : corriger le nombre de techniciens utilisés pour qu'il ne soit pas celui des jours/techniciens
-            //bw.write("NUMBER_OF_TECHNICIANS_USED = " + solution.getNbTechniciansUsed() + "\n");
+            bw.write("NUMBER_OF_TECHNICIANS_USED = " + solution.getNbTechniciansUsed() + "\n");
             bw.write("IDLE_MACHINE_COSTS = " + solution.getIdleMachineCosts() + "\n");
             bw.write("TOTAL_COST = " + solution.getTotalCost() + "\n");
             /*
